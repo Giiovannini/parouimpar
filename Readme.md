0270af74a8f82f3abb002fef7aa7fa77c5fffbbb
Giovannini Barbosa <giovanninibaarbosa@gmail.com>
2017-10-16 20:40:04 -0200

    First commmit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+Programa criado para verificar se o número inserido é par ou ímpar.
+
+O programa verifica se é par ou ímpar através do resto da divisão do número inserido por dois.
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero;
+
+    cout << "Digite um numero: ";
+    cin >> numero;
+    if (numero % 2 ==0){
+        cout << "O numero " << numero << " e par" << endl;
+
+    } else {
+
+        cout << "O numero " << numero << "e impar" << endl;
+
+    }
+
+    return 0;
+}
